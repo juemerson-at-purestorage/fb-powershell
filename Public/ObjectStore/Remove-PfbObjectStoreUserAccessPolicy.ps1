a09cdb5fb8367716fa5e940c91c5e51116572a6d
Justin Emerson <Justin.Emerson@everpuredata.com>
2026-07-02 19:19:56 -0700

    Fix ObjectStore access-policy association pipeline binding (flatten Get, pipe-bind New/Remove)

--- Public/ObjectStore/Remove-PfbObjectStoreUserAccessPolicy.ps1
+++ Public/ObjectStore/Remove-PfbObjectStoreUserAccessPolicy.ps1
@@ -4,44 +4,44 @@ function Remove-PfbObjectStoreUserAccessPolicy {
         Removes the link between an object store user and an access policy.
     .DESCRIPTION
         Deletes the association between an object store user and an access
-        policy. The user will no longer inherit the permissions defined by
-        that policy.
+        policy. Accepts flattened association objects from
+        Get-PfbObjectStoreUserAccessPolicy on the pipeline.
     .PARAMETER MemberName
-        The name of the object store user (account/user format).
+        The name of the object store user (account/user format). Binds from pipeline property 'MemberName'.
     .PARAMETER PolicyName
-        The name of the access policy to unlink.
+        The name of the access policy to unlink. Binds from pipeline property 'PolicyName'.
     .PARAMETER Array
         The FlashBlade connection object.
     .EXAMPLE
         Remove-PfbObjectStoreUserAccessPolicy -MemberName "acct1/user1" -PolicyName "full-access-policy"
         Removes the link between the user and the access policy.
     .EXAMPLE
-        Remove-PfbObjectStoreUserAccessPolicy -MemberName "acct1/temp-user" -PolicyName "temp-policy"
-        Unlinks a temporary policy from its user.
-    .EXAMPLE
-        Get-PfbObjectStoreUserAccessPolicy -MemberName "acct1/old-user" |
-            ForEach-Object { Remove-PfbObjectStoreUserAccessPolicy -MemberName $_.member.name -PolicyName $_.policy.name }
+        Get-PfbObjectStoreUserAccessPolicy -MemberName "acct1/old-user" | Remove-PfbObjectStoreUserAccessPolicy
         Removes all access policy links from a user.
     #>
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
     param(
-        [Parameter(Mandatory, Position = 0)]
+        [Parameter(Mandatory, Position = 0, ValueFromPipelineByPropertyName)]
         [string]$MemberName,
 
-        [Parameter(Mandatory, Position = 1)]
+        [Parameter(Mandatory, Position = 1, ValueFromPipelineByPropertyName)]
         [string]$PolicyName,
 
         [Parameter()] [PSCustomObject]$Array
     )
 
-    Assert-PfbConnection -Array ([ref]$Array)
-
-    $queryParams = @{
-        'member_names' = $MemberName
-        'policy_names' = $PolicyName
+    begin {
+        Assert-PfbConnection -Array ([ref]$Array)
     }
 
-    if ($PSCmdlet.ShouldProcess("User=$MemberName, Policy=$PolicyName", 'Remove user access policy link')) {
-        Invoke-PfbApiRequest -Array $Array -Method DELETE -Endpoint 'object-store-users/object-store-access-policies' -QueryParams $queryParams
+    process {
+        $queryParams = @{
+            'member_names' = $MemberName
+            'policy_names' = $PolicyName
+        }
+
+        if ($PSCmdlet.ShouldProcess("User=$MemberName, Policy=$PolicyName", 'Remove user access policy link')) {
+            Invoke-PfbApiRequest -Array $Array -Method DELETE -Endpoint 'object-store-users/object-store-access-policies' -QueryParams $queryParams
+        }
     }
 }
